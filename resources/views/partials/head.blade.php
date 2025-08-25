0aedb383ecc32bb8bdab823db6df9e39ac012914
Sacrudyme <bladsparrow74@gmail.com>
2025-08-25 21:16:00 +0800

    sadsadsaffg

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,13 +5,12 @@
 
 <link rel="icon" href="{{ asset('/favicon.ico') }}" sizes="any">
 <link rel="icon" href="{{ asset('/favicon.svg') }}" type="image/svg+xml">
-<link rel="apple-touch-icon" href="{{ asset('/apple-touch-icon.png') }}">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-{{-- Fonts (optional) --}}
-{{-- 
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> 
---}}
+<!-- <link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> -->
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+@fluxAppearance
 @livewireStyles
+
